f899d1f85dbc83d61ca895d981b311dd6f857bce
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-19 11:24:53 -1000

    Test for award_spoils

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -304,7 +304,7 @@ class CardTest < Minitest::Test
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
-    assert_equal "Megan" , turn.winner
+    assert_equal player1 , turn.winner
   end
 
   def test_turn_war_type
@@ -405,6 +405,57 @@ class CardTest < Minitest::Test
     assert_equal [], player1.deck.cards
     assert_equal [], player2.deck.cards
   end
+
+  def test_award_spoils_for_player1_basic_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:diamond, '7', 7)
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal 2, player1.deck.cards.count
+  end
+
+  def test_award_spoils_for_player1_war_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    deck1 = Deck.new([card1,card2,card3])
+    deck2 = Deck.new([card4,card5,card6])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal 6, player2.deck.cards.size
+    assert_equal [], turn.spoils_of_war
+  end
+
+  # def test_award_spoils_for_mutually_assured_destruction
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '8', 8)
+  #   card3 = Card.new(:heart, 'Jack', 11)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:diamond, '7', 7)
+  #   card6 = Card.new(:diamond, 'Jack', 11)
+  #   deck1 = Deck.new([card1,card2,card3])
+  #   deck2 = Deck.new([card4,card5,card6])
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #   turn = Turn.new(player1, player2)
+  #   turn.winner
+  #   turn.pile_cards
+  #   assert_equal [], turn.spoils_of_war
 
 
   #add test case for player1 has too few cards
