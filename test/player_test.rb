1b8afc1591a8b4e7b7094b962ea39df568a7f262
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-20 08:55:46 -1000

    Add player_test

---
+++ test/player_test.rb
@@ -0,0 +1,131 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+class PlayerTest < Minitest::Test
+
+  def test_instance_of_player
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+
+  def test_player_name
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal "Clarisa", player.name
+  end
+
+  def test_player_has_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert player.deck
+  end
+
+  def test_player_starts_not_losing
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    refute player.has_lost?
+  end
+
+  def test_remove_card_for_player
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal :diamond, player.deck.remove_card.suit
+  end
+
+  def test_player_did_not_lose_after_removing_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+
+    refute player.has_lost?
+  end
+
+  def test_player_removed_another_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    assert_equal :spade, player.deck.remove_card.suit
+  end
+
+  def test_player_did_not_lose_after_removing_two_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+
+    refute player.has_lost?
+  end
+
+  def test_player_removes_third_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+
+    assert_equal :heart, player.deck.remove_card.suit
+  end
+
+  def test_player_did_not_lose_after_removing_three_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    assert player.has_lost?
+  end
+
+  def test_player_has_no_cards_left
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    assert_equal [], player.deck.cards
+  end
+
+end
